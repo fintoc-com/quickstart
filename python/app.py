74dcf40f830317136f80ea4b05f22e1678260446
Christian Melendez <camelendez@uc.cl>
2021-05-18 17:56:42 -0400

    Add get accounts endpoint

--- python/app.py
+++ python/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request
+from fintoc import Client
+from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
+client = Client(os.environ['SECRET_KEY'])
 link_token = None
 
 
@@ -10,3 +13,20 @@ def set_link_token():
     global link_token
     link_token = request.json['data']['link_token']
     return 'link token set'
+
+
+@app.route('/api/accounts', methods=['GET'])
+def get_accounts():
+    def transform_account(account):
+        return {
+            'id': account.id_,
+            'name': account.name,
+            'holderName': account.holder_name,
+            'currency': account.currency,
+            'balance':  {
+                'available': account.balance.available,
+                'current': account.balance.current,
+            }
+        }
+    link = client.get_link(link_token)
+    return jsonify([transform_account(account) for account in link])
