51e9ce2d3a9ae0f6fb5e7880d544227f8ad15924
Christian Melendez <camelendez@uc.cl>
2021-05-18 20:17:39 -0400

    Add get account movements endpoint

--- python/app.py
+++ python/app.py
@@ -1,3 +1,4 @@
+from datetime import date
 from fintoc import Client
 from flask import Flask, request, jsonify
 import os
@@ -30,3 +31,20 @@ def get_accounts():
         }
     link = client.get_link(link_token)
     return jsonify([transform_account(account) for account in link])
+
+
+@app.route('/api/accounts/<account_id>/movements', methods=['GET'])
+def get_account_movements(account_id):
+    def transform_movement(movement):
+        return {
+            'id': movement.id_,
+            'postDate': movement.post_date.isoformat(),
+            'amount': movement.amount,
+            'currency': movement.currency,
+            'description': movement.description,
+        }
+    current_month = date.today().replace(day=1)
+    link = client.get_link(link_token)
+    account = link.find(id_=account_id)
+    movements = account.get_movements(since=current_month)
+    return jsonify([transform_movement(movement) for movement in movements])
